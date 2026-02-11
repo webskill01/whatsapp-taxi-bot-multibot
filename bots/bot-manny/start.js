c73a1f18fe805be103e91f9c06cf986ffb787055
Nitin <webskillset@gmail.com>
2026-02-11 20:53:20 +0530

    fixed

--- bots/bot-manny/start.js
+++ bots/bot-manny/start.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
 import path from "path";
 import { fileURLToPath } from "url";
-import "../../core/index.js";
+
+// --------------------------------------------------
+// SET BOT DIR ARG **BEFORE** CORE IS IMPORTED
+// --------------------------------------------------
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Pass bot dir via argv (REQUIRED)
+// Inject bot directory for core
 process.argv[2] = __dirname;
+
+// NOW import core (it will read argv[2] correctly)
+await import("../../core/index.js");
